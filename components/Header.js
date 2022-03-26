3cad3f88e0b0a2d1452a8c1607f32ce7df0305c2
ossaili <shadi.ossaili@yandex.com>
2022-03-26 18:49:29 +0200

    1 binaries uploaded

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+export default function Header() {
+  return (
+    <header className="">
+      <Image src="https://links.papareact.com/ua6" width={200} height={100} />
+    </header>
+  );
+}
